21dcc8e5c6ecbec79899d81e7a78c3ee48e1d494
Bojan Mihelac <bmihelac@mihelac.org>
2010-03-06 21:58:30 +0100

    Make navigation_type_choices defaults and read them from settings.py

--- feincmsext/extended_navigation/extensions/navigation_type.py
+++ feincmsext/extended_navigation/extensions/navigation_type.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 
-# TODO: check if this is set in settings
-NAVIGATION_TYPE_CHOICES = (
+DEFAULT_NAVIGATION_TYPE_CHOICES = (
     ('primary_links', _('primary links')),
     ('secondary_links', _('secondary links')),
 )
+navigation_type_choices = getattr(settings, "NAVIGATION_TYPE_CHOICES", DEFAULT_NAVIGATION_TYPE_CHOICES)
 
 def register(cls, admin_cls):
     cls.add_to_class('navigation_type', models.CharField(_('navigation type'),
-        max_length=32, choices=NAVIGATION_TYPE_CHOICES))
+        max_length=32, choices=navigation_type_choices))
