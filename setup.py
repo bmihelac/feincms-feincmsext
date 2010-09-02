4f06edbcc3c04b9acfb594abf51d9e820aef35ab
Bojan Mihelac <bmihelac@mihelac.org>
2010-09-02 09:32:23 +0200

    bumb version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "FeinCMS Extensions",
-    version = "0.1",
+    version = "0.1.1",
     author="Bojan Mihelac",
     author_email='bmihelac@mihelac.org',
     url='http://github.com/bmihelac/feincms-feincmsext',
