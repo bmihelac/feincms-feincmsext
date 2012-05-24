f437c4007ffde85995cf8b7098c2cf15e0667139
Bojan Mihelac <bmihelac@mihelac.org>
2012-05-24 14:20:29 +0200

    Add create_content_types.

---
+++ feincmsext/util/content_types.py
@@ -0,0 +1,52 @@
+from django.utils.importlib import import_module
+from django.core.exceptions import ImproperlyConfigured
+
+
+def load_class(class_path):
+    """
+    Loads a class given a class_path.
+    """
+    class_module, class_name = class_path.rsplit('.', 1)
+    mod = import_module(class_module)
+    clazz = getattr(mod, class_name)
+    return clazz
+
+
+def create_content_types(cls, content_types_conf):
+    """
+    Create content types for given ``content_types_conf``.
+
+    ``content_types_conf`` is a list or tuple, each element should have
+    content type configuration. 
+
+    Content type configuration is list or tuple with following elements:
+
+    * content_type - class path or model (can be list)
+    * region - region (optional, can be list)
+    * options - dictionary to pass as options (optional)
+
+    >>> content_types = [
+            (RichTextContent, ), # all regions
+            (
+                ('feincms.content.video.models', MediaFileContent), # multiple content types
+                ('main', 'sidebar'), # multiple regions
+                {'TYPE_CHOICES': (('block', 'block'),), # options
+            )
+            ]
+    >>> create_content_type(Page, content_types)
+    """
+    for conf in content_types_conf:
+        content_types = conf[0]
+        if not isinstance(content_types, (list, tuple,)):
+            content_types = (content_types,)
+        regions = conf[1] if len(conf) == 2 else None
+        kwargs = conf[2] if len(conf) == 3 else {}
+        if regions:
+            kwargs['regions'] = regions
+        for content_type in content_types:
+            if isinstance(content_type, basestring):
+                try:
+                    content_type = load_class(content_type)
+                except Exception:
+                    raise ImproperlyConfigured("Error importing %s" % content_type)
+            cls.create_content_type(content_type, **kwargs)
