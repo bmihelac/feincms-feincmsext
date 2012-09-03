5fa04cce9ac4ac65aa476b0f7efba71906fa474c
Bojan Mihelac <bmihelac@mihelac.org>
2012-09-03 17:46:06 +0200

    PEP8 cleanup

--- feincmsext/haystack/templatetags/search_helper_tags.py
+++ feincmsext/haystack/templatetags/search_helper_tags.py
@@ -8,16 +8,16 @@ def page_search_result(page):
     """
     Return content in main region for display in search result.
     """
-    return "".join([c.render() for c in page.content.main])
+    return u"".join([c.render() for c in page.content.main])
 
 
 @register.filter
 def search_include(result):
     """
-    Returns filename to use for search inclusion template for ``result`` object.
+    Returns filename to use for search inclusion template for
+    ``result`` object.
     """
     ct = result.content_type()
     args = ct.split('.')
     template_name = "search/_%s_%s.html" % tuple(args)
     return template_name
-
