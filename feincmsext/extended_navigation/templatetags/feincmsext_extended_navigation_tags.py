574c467f6d9450515249a179722cb291af7cc9fc
Bojan Mihelac <bmihelac@mihelac.org>
2012-09-10 14:16:49 +0200

    PEP8 cleanup

--- feincmsext/extended_navigation/templatetags/feincmsext_extended_navigation_tags.py
+++ feincmsext/extended_navigation/templatetags/feincmsext_extended_navigation_tags.py
@@ -1,9 +1,7 @@
-from django.core.urlresolvers import reverse
 from django import template
-from django.conf import settings
 
 from templatetag_sugar.register import tag
-from templatetag_sugar.parser import Name, Variable, Constant, Optional, Model
+from templatetag_sugar.parser import Name, Variable, Constant, Optional
 
 from feincms.module.page.models import Page, PageManager
 
@@ -14,21 +12,23 @@ register = template.Library()
 
 
 def get_navigation(start_page=None, level=0, depth=1, active_depth=0,
-        language=None, navigation_type=None, extended=False, 
+        language=None, navigation_type=None, extended=False,
         request=None):
     if not start_page:
         root = Page.objects
     else:
         if (start_page.level < level - 1):
             return []
-        if start_page.is_root_node() or (start_page.level == level-1):
+        if start_page.is_root_node() or (start_page.level == level - 1):
             root_page = start_page
         else:
-            root_page = start_page.get_ancestors().get(level=max(level-1, 0))
-        root = Page.objects.filter(lft__gte=root_page.lft, rght__lte=root_page.rght, tree_id=root_page.tree_id)
+            root_page = start_page.get_ancestors().get(level=max(level - 1, 0))
+        root = Page.objects.filter(lft__gte=root_page.lft,
+                rght__lte=root_page.rght, tree_id=root_page.tree_id)
     from_level = level
     to_level = level + depth
-    queryset = root.filter(level__gte=from_level, level__lt=to_level, in_navigation=True)
+    queryset = root.filter(level__gte=from_level,
+            level__lt=to_level, in_navigation=True)
     if language:
         queryset = queryset.filter(language=language)
     if navigation_type:
@@ -40,42 +40,46 @@ def get_navigation(start_page=None, level=0, depth=1, active_depth=0,
             active_node = start_page
         else:
             try:
-                active_node = start_page.get_ancestors().get(in_navigation=True, level=level)
+                active_node = start_page.get_ancestors().get(
+                        in_navigation=True, level=level)
             except type(start_page).DoesNotExist:
                 # active_node is not in navigation
                 active_node = None
         # handle case when start_page is not in navigation
         if active_node in entries:
             index = entries.index(active_node) + 1
-            entries[index:index] = active_node.children.filter(in_navigation=True).filter(level__lte=level + active_depth)
+            entries[index:index] = active_node.children.filter(
+                    in_navigation=True).filter(level__lte=level + active_depth)
     if extended:
         _entries = list(entries)
         entries = []
         # add extended navigation from root_page
         if getattr(root_page, 'navigation_extension', None):
-            extended_entries = [p for p in root_page.extended_navigation(level=root_page.level+1, 
+            extended_entries = [p for p in root_page.extended_navigation(
+                level=root_page.level + 1,
                 tree_id=root_page.tree_id, lft=0, rght=0,
-                request=request) if p.level <= root_page.level+depth]
-            entries.extend(extended_entries)        
+                request=request) if p.level <= root_page.level + depth]
+            entries.extend(extended_entries)
         # and from all entries
         for entry in _entries:
             entries.append(entry)
             if getattr(entry, 'navigation_extension', None):
-                extended_entries = [p for p in entry.extended_navigation(level=entry.level+1, 
+                extended_entries = [p for p in entry.extended_navigation(
+                    level=entry.level + 1,
                     tree_id=root_page.tree_id, lft=0, rght=0,
-                    request=request) if p.level <= root_page.level+depth]
-                entries.extend(extended_entries)        
+                    request=request) if p.level <= root_page.level + depth]
+                entries.extend(extended_entries)
     return entries
 
 
-@tag(register, [Optional([Constant("for"), Variable("start_page")]), 
-                Optional([Constant("level"), Variable("level")]), 
-                Optional([Constant("depth"), Variable("depth")]), 
-                Optional([Constant("active_depth"), Variable("active_depth")]), 
-                Optional([Constant("language"), Variable("language")]), 
-                Optional([Constant("type"), Variable("navigation_type")]), 
-                Optional([Constant("extended"), Variable("extended")]), 
-                Optional([Constant("request"), Variable("request")]), 
+@tag(register, [Optional([Constant("for"), Variable("start_page")]),
+                Optional([Constant("level"), Variable("level")]),
+                Optional([Constant("depth"), Variable("depth")]),
+                Optional([Constant("active_depth"), Variable("active_depth")]),
+                Optional([Constant("language"), Variable("language")]),
+                Optional([Constant("type"), Variable("navigation_type")]),
+                Optional([Constant("extended"), Variable("extended")]),
+                Optional([Constant("request"), Variable("request")]),
                 Optional([Constant("as"), Name('asvar')])
                 ])
 def extended_navigation(context, start_page=None, level=0, depth=1,
@@ -83,8 +87,8 @@ def extended_navigation(context, start_page=None, level=0, depth=1,
         extended=False, request=None, asvar=None):
     if isinstance(start_page, basestring):
         start_page = Page.objects.get(title=start_page)
-    entries = get_navigation(start_page=start_page, level=level, 
-                depth=depth, active_depth=active_depth, language=language, 
+    entries = get_navigation(start_page=start_page, level=level,
+                depth=depth, active_depth=active_depth, language=language,
                 extended=extended, navigation_type=navigation_type,
                 request=request)
     if asvar:
@@ -94,13 +98,14 @@ def extended_navigation(context, start_page=None, level=0, depth=1,
         arr = []
         arr.append('<ul>')
         for page in entries:
-            arr.append('<li><a href="%s">%s</a></li>' % (page.get_absolute_url(), page.title))
+            arr.append('<li><a href="%s">%s</a></li>' % (
+                page.get_absolute_url(), page.title))
         arr.append('</ul>')
         return '\n'.join(arr)
 
 
-@tag(register, [Constant("for"), Variable("start_page"), 
-                Constant("level"), Variable("level"), 
+@tag(register, [Constant("for"), Variable("start_page"),
+                Constant("level"), Variable("level"),
                 Constant("as"), Name('asvar')
                 ])
 def parent_feincms_page(context, start_page, level, asvar):
@@ -115,16 +120,19 @@ def parent_feincms_page(context, start_page, level, asvar):
     return ""
 
 
-@tag(register, [Variable("contents"), 
-                Variable("expr"), 
+@tag(register, [Variable("contents"),
+                Variable("expr"),
                 Constant("as"), Name('asvar')
                 ])
 def group_page_content(context, contents, expr, asvar):
     """
     Allows grouping feincms page contents with regular expressions.
-    
-        {% group_page_content feincms_page.content.main "[richtext]{2}" as content_groups %}
-        
+
+        {% group_page_content feincms_page.content.main "[richtext]{2}"
+        as content_groups %}
+
     """
-    context[asvar] = regex_group_list(contents, expr, lambda x: x._meta.module_name)
+    context[asvar] = regex_group_list(contents,
+            expr,
+            lambda x: x._meta.module_name)
     return ''
